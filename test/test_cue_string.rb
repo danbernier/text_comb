ba91e5432df9c2d75468f3ddbaeb9ed2e0f4352d
Dan Bernier <danbernier@gmail.com>
2011-09-08 13:11:20 -0400

    Added tests for extending a string directly, by combining them with the Cue::String tests, via a mix-in

--- test/test_cue_string.rb
+++ test/test_cue_string.rb
@@ -1,20 +1,28 @@
 require 'cue/ext'
 
-class TestCueString < Test::Unit::TestCase
+module CueStringTests
 
-  def test_can_call_normal_String_methods
+  def test_can_call_normal_string_methods
     plain_string = "I came. I saw. I hacked."
-    cue_string = Cue::String.new(plain_string)
+    cue_string = cue_string(plain_string)
 
     assert_equal plain_string.upcase, cue_string.upcase
     assert_equal plain_string.reverse, cue_string.reverse
     assert_equal plain_string.gsub(/i/i, "We"), cue_string.gsub(/i/i, "We")
   end
 
+  def test_plain_strings_do_not_have_cue_methods
+    cue_string = cue_string("I came. I saw. I hacked.")
+
+    [:each_word, :each_sentence, :each_ngram].each do |method|
+      assert !"".respond_to?(method)
+    end
+  end
+
   def test_can_call_each_word
     expected = %w[I came I saw I hacked]
 
-    cuestr = Cue::String.new("I came. I saw. I hacked.")
+    cuestr = cue_string("I came. I saw. I hacked.")
     actual = cuestr.enum_for(:each_word).to_a
 
     assert_equal expected, actual
@@ -23,7 +31,7 @@ class TestCueString < Test::Unit::TestCase
   def test_can_call_each_sentence
     expected = ["I came. ", "I saw. ", "I hacked."]
 
-    cuestr = Cue::String.new("I came. I saw. I hacked.")
+    cuestr = cue_string("I came. I saw. I hacked.")
     actual = cuestr.enum_for(:each_sentence).to_a
 
     assert_equal expected, actual
@@ -32,10 +40,26 @@ class TestCueString < Test::Unit::TestCase
   def test_can_call_each_ngram
     expected = ["Never wake a", "wake a sleeping", "a sleeping cat"]
 
-    cuestr = Cue::String.new("Never wake a sleeping cat.")
+    cuestr = cue_string("Never wake a sleeping cat.")
     actual = []
     cuestr.each_ngram(3) { |ng| actual.push ng }
 
     assert_equal expected, actual
+  end
+end
+
+class TestCueStringNew < Test::Unit::TestCase
+  include CueStringTests
+
+  def cue_string(plain_string)
+    Cue::String.new(plain_string)
+  end
+end
+
+class TestStringExtensions < Test::Unit::TestCase
+  include CueStringTests
+
+  def cue_string(plain_string)
+    plain_string.extend(Cue::StringExtensions)
   end
 end
--- test/test_string_extensions.rb
+++
@@ -1,6 +0,0 @@
-require 'cue/ext'
-
-class TestStringExtensions < Test::Unit::TestCase
-  def test_foo
-  end
-end
