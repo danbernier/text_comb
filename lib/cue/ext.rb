bbd61d93268b82eaa3d9bfc2fd1dbfa02a068725
Dan Bernier <danbernier@gmail.com>
2011-09-10 12:31:11 -0400

    Made Cue::String's blocks all optional

--- lib/cue/ext.rb
+++ lib/cue/ext.rb
@@ -3,16 +3,29 @@ require 'delegate'
 module Cue
   module StringExtensions
     def each_word(&block)
-      Cue.each_word(self.to_s, &block)
+      if block_given?
+        Cue.each_word(self.to_s, &block)
+      else
+        to_enum(:each_word)
+      end
     end
 
     def each_sentence(&block)
-      Cue.each_sentence(self.to_s, &block)
+      if block_given?
+        Cue.each_sentence(self.to_s, &block)
+      else
+        to_enum(:each_sentence)
+      end
     end
 
     def each_ngram(n, &block)
-      Cue.each_ngram(self.to_s, n, &block)
+      if block_given?
+        Cue.each_ngram(self.to_s, n, &block)
+      else
+        to_enum(:each_ngram, n)
+      end
     end
+    
   end
 
   class String < DelegateClass(::String)
