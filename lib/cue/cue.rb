8f4b3c2ebe0de8785021ccbdf2b4a9ccb45d7273
Dan Bernier <danbernier@gmail.com>
2011-09-10 16:46:50 -0400

    Made blocks optional for Cue.each_* methods.

--- lib/cue/cue.rb
+++ lib/cue/cue.rb
@@ -6,30 +6,51 @@ require '../bin/cue.language.jar'
 
 
 module Cue
-  
-  def self.each_word(string)
-    iter = Java::CueLang::WordIterator.new(string)
-    
-    while iter.has_next do
-      yield iter.next
+
+  def self.each_word(string, &block)
+    iter = CueEnumerator.new Java::CueLang::WordIterator.new(string)
+
+    if block_given?
+      iter.each &block
+    else
+      iter
     end
   end
 
-  def self.each_sentence(string)
-    iter = Java::CueLang::SentenceIterator.new(string)
-    
-    while iter.has_next do
-      yield iter.next
+  def self.each_sentence(string, &block)
+    iter = CueEnumerator.new Java::CueLang::SentenceIterator.new(string)
+
+    if block_given?
+      iter.each &block
+    else
+      iter
     end
   end
 
   # TODO allow for Locale, and custom StopWords
-  def self.each_ngram(string, n)
-    iter = Java::CueLang::NGramIterator.new(n, string)
-    
-    while iter.has_next do
-      yield iter.next
+  def self.each_ngram(string, n, &block)
+    iter = CueEnumerator.new Java::CueLang::NGramIterator.new(n, string)
+
+    if block_given?
+      iter.each &block
+    else
+      iter
     end
+  end
+
+  class CueEnumerator
+    include Enumerable
+
+    def initialize(java_iter)
+      @java_iter = java_iter
+    end
+
+    def each
+      while @java_iter.has_next
+        yield @java_iter.next
+      end
+    end
+
   end
 
   # TODO add StopWords.guess()
--- test/test_java_interface.rb
+++ test/test_java_interface.rb
@@ -10,7 +10,7 @@ class TestJavaInterface < Test::Unit::TestCase
      will be nothing.  Only I will remain."
   end
 
-  def test_each_word
+  def test_each_word_with_block
     words = []
     Cue.each_word("I must not fear. ") { |word| words.push word }
 
@@ -21,7 +21,17 @@ class TestJavaInterface < Test::Unit::TestCase
     end
   end
 
-  def test_each_sentence
+  def test_each_word_without_block
+    words = Cue.each_word("I must not fear. ")
+
+    expected_words = %w[I must not fear]
+
+    expected_words.zip(words.to_a).each do |actual, expected|
+      assert_equal expected, actual
+    end
+  end
+
+  def test_each_sentence_with_block
     sentences = []
     Cue.each_sentence(littany_against_fear) { |s| sentences.push s }
 
@@ -41,9 +51,36 @@ class TestJavaInterface < Test::Unit::TestCase
     end
   end
 
-  def test_each_ngram
+  def test_each_sentence_without_block
+    sentences = Cue.each_sentence(littany_against_fear)
+
+    expected_sentences = [
+      "I must not fear. ",
+      "Fear is the mind-killer. ",
+      "Fear is the little-death that brings total obliteration. ",
+      "I will face my fear. ",
+      "I will permit it to pass over me and through me. ",
+      "And when it has gone past I will turn the inner eye to see its path. ",
+      "Where the fear has gone there will be nothing. ",
+      "Only I will remain."
+    ]
+
+    expected_sentences.zip(sentences.to_a).each do |expected, actual|
+      assert_equal expected, actual
+    end
+  end
+
+  def test_each_ngram_with_block
     ngrams = []
     Cue.each_ngram("I must not fear. ", 2) { |ngram| ngrams.push ngram }
+
+    assert_equal "I must", ngrams.shift
+    assert_equal "must not", ngrams.shift
+    assert_equal "not fear", ngrams.shift
+  end
+
+  def test_each_ngram_without_block
+    ngrams = Cue.each_ngram("I must not fear. ", 2).to_a
 
     assert_equal "I must", ngrams.shift
     assert_equal "must not", ngrams.shift
